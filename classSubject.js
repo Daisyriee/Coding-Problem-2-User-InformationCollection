3962cfdf889fe3acf330cfe3f177da1968b473e2
Marvi Fernando <syriefernando@gmail.com>
2024-11-04 23:38:53 +0800

    Initial commit

---
+++ classSubject.js
@@ -0,0 +1,8 @@
+let subjectTitle = "Discrete Structures Algorithms";
+let classSchedule = "9:30, Monday and Friday";
+let classroom = "CLB";
+let classInstructor = "Sir Vitruvius John D. Barayuga";
+let studentName = "Daisyrie F. Fernando";
+
+let script = studentName + " " + "is currently enrolled in" + " " + subjectTitle + " " + "with a class schedule of" + " " + classSchedule + " " + "at room" + " " + classroom + "." + " " + "The instructor for the subject is" + " " + classInstructor;
+console.log(script)
